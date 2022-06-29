bdb84cbbaa3792ef70e92e898f431485156b57fa
Henriqueba210 <henrique.balmeida@outlook.com>
2022-06-29 07:33:49 +0100

    refactor: :recycle: created initial files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import "./styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
